5b0db11582308e01d44e8853338c3301af767249
Benaissa Znnati <znnati@outlook.com>
2021-10-11 22:03:47 +0200

    Added ignored log project.

---
+++ Thesaurus.Log/IThesaurusLog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Thesaurus.Log
+{
+	public interface IThesaurusLog
+	{
+		void Write(string message);
+
+		void Write(string message, Exception exception);
+	}
+}
---
+++ Thesaurus.Log/ThesaurusLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using log4net;
+
+namespace Thesaurus.Log
+{
+	public class ThesaurusLog : IThesaurusLog
+	{
+		private readonly ILog log;
+
+		public ThesaurusLog(string name)
+		{
+			log = LogManager.GetLogger(name);
+		}
+
+		public void Write(string message)
+		{
+			log.Info(message);
+		}
+
+		public void Write(string message, Exception exception)
+		{
+			log.Info(message, exception);
+		}
+	}
+}
